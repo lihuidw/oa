6507e5cd8e7da5a9018a1595df005c926240d11d
admin <admin@stu775344>
2018-10-17 19:36:49 +0800

    day01

--- src/main/java/com/bwf/dao/IUserDao.java
+++
@@ -1,8 +0,0 @@
-package com.bwf.dao;
-
-import com.bwf.entity.User;
-
-public interface IUserDao {
-
-	User login( User user );
-}
---
+++ src/main/java/com/bwf/dao/UserMapper.java
@@ -0,0 +1,15 @@
+package com.bwf.dao;
+
+import com.bwf.entity.User;
+
+public interface UserMapper {
+	User getUserById(int id);
+	
+	void add(User user);
+	void delete(int id);
+	void update(User user);
+	User login( User user );
+	
+
+	User getUserByUsernameAndPassword(User user);
+}
