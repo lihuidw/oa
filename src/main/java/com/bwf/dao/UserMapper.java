e517f67299cef06b913025ff1a5bfd87063214e6
admin <admin@stu775344>
2018-10-20 16:39:54 +0800

    day03

--- src/main/java/com/bwf/dao/UserMapper.java
+++ src/main/java/com/bwf/dao/UserMapper.java
@@ -1,15 +1,28 @@
 package com.bwf.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.bwf.entity.User;
 
 public interface UserMapper {
 	User getUserById(int id);
 	
-	void add(User user);
-	void delete(int id);
+	void add(User user1);
+	
 	void update(User user);
+	
 	User login( User user );
 	
 
 	User getUserByUsernameAndPassword(User user);
+
+	User getMenusAndOperatesByUserId(int userId);
+
+	List<User> getAllUsers();
+
+	void deleteUser(Integer id);
+
+	void deleteMulti(@Param("idArr") Integer[] idArr);
 }
